0bbceae31695333406b74869d06248d15f81202b
Jean Stiletto <fabian@nordwiesen30.de>
2026-08-26 18:11:08 +0200

    login tool: publish v3.2 (rolling 'login-tool')

--- installer/SkuInstaller/Config.cs
+++ installer/SkuInstaller/Config.cs
@@ -122,7 +122,7 @@ namespace SkuInstaller
         // whenever a newer tool build is published, and LoginToolInstaller upgrades
         // in place any user whose recorded marker predates it.
         public const string LoginToolTag = "login-tool";
-        public const string LoginToolVersion = "3.1";
+        public const string LoginToolVersion = "3.2";
         public const string LoginToolAsset = "WoW-Login-Tool.zip";
         public const string LoginToolFolderName = "WoW Login Tool";
 
